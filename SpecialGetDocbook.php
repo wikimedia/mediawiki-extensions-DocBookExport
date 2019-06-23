eeb61ee236682cd04f65272e83c21dde0ebc2895
Nischayn22 <nischayn22@gmail.com>
2019-06-23 14:09:02 +0530

    minor fix
    Change-Id: I9999912faee2fede96a869c2183909494159d71c

--- SpecialGetDocbook.php
+++ SpecialGetDocbook.php
@@ -52,7 +52,7 @@ class SpecialGetDocbook extends SpecialPage {
 
 		$book_title = '';
 		$book_contents = '<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.1.2//EN" "http://www.oasis-open.org/docbook/xml/4.1.2/docbookx.dtd">
-		<book>';
+		<book xmlns:xlink="http://www.w3.org/1999/xlink">';
 
 		$all_files = array();
 		$index_terms = array();
