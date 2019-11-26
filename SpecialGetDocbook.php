cf64f30d66e5b98c75a78b2f74457f7af40e7069
Nischayn22 <nischayn22@gmail.com>
2019-11-26 13:00:29 +0530

    Added cleansing to make ODT work
    Change-Id: I28b73f44e47ce82213f7abf6db0c429a1f4e998c

--- SpecialGetDocbook.php
+++ SpecialGetDocbook.php
@@ -704,6 +704,7 @@ class SpecialGetDocbook extends SpecialPage {
 
 		$html = str_replace( "</html>", "", $html );
 		$html = str_replace( "<html>", "", $html );
+		$html = cleanse( $html );
 
 		return $html;
 	}
@@ -859,4 +860,66 @@ function rrmdir($dir) {
      }
      rmdir($dir);
    } 
+}
+
+function cleanse($string, $allowedTags = array()) {
+	if (get_magic_quotes_gpc()) {
+		$string = stripslashes($stringIn);
+	}
+	
+	// ============
+	// Remove MS Word Special Characters
+	// ============
+	
+	$search  = array('&acirc;€“','&acirc;€œ','&acirc;€˜','&acirc;€™','&Acirc;&pound;','&Acirc;&not;','&acirc;„&cent;');
+	$replace = array('-','&ldquo;','&lsquo;','&rsquo;','&pound;','&not;','&#8482;');
+	
+	$string = str_replace($search, $replace, $string);
+	$string = str_replace('&acirc;€', '&rdquo;', $string);
+
+	$search = array("&#39;", "\xc3\xa2\xc2\x80\xc2\x99", "\xc3\xa2\xc2\x80\xc2\x93", "\xc3\xa2\xc2\x80\xc2\x9d", "\xc3\xa2\x3f\x3f");
+	$resplace = array("'", "'", ' - ', '"', "'");
+	
+	$string = str_replace($search, $replace, $string);
+
+	$quotes = array(
+		"\xC2\xAB"     => '"',
+		"\xC2\xBB"     => '"',
+		"\xE2\x80\x98" => "'",
+		"\xE2\x80\x99" => "'",
+		"\xE2\x80\x9A" => "'",
+		"\xE2\x80\x9B" => "'",
+		"\xE2\x80\x9C" => '"',
+		"\xE2\x80\x9D" => '"',
+		"\xE2\x80\x9E" => '"',
+		"\xE2\x80\x9F" => '"',
+		"\xE2\x80\xB9" => "'",
+		"\xE2\x80\xBA" => "'",
+		"\xe2\x80\x93" => "-",
+		"\xc2\xb0"	   => "°",
+		"\xc2\xba"     => "°",
+		"\xc3\xb1"	   => "&#241;",
+		"\x96"		   => "&#241;",
+		"\xe2\x81\x83" => '&bull;'
+	);
+	$string = strtr($string, $quotes);
+	/*
+	// Use the below to get the byte of the special char and put it in the array above + the replacement.
+	
+	if (strpos($string, "Live Wave Buoy Data") !== false)
+	{
+		for ($i=strpos($string, "Live Wave Buoy Data") ; $i<strlen($string) ; $i++) {
+			$byte = $string[$i];
+			$char = ord($byte);
+			printf('%s:0x%02x ', $byte, $char);
+		}
+	}
+	var_dump($string);
+	exit;
+	*/
+	// ============
+	// END
+	// ============
+	
+	return $string;
 }
