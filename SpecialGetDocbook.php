899360a55ffa23f4a87faed749e15815a6146d29
Nischayn22 <nischayn22@gmail.com>
2020-11-04 19:01:46 +0530

    re added xlink declaration
    Change-Id: Ic41ee474dd47094528406e3af37395e2732de7ec

--- SpecialGetDocbook.php
+++ SpecialGetDocbook.php
@@ -71,7 +71,7 @@ class SpecialGetDocbook extends SpecialPage {
 		}
 
 		$book_contents = '<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">
-		<book lang="en">';
+		<book lang="en" xmlns:xlink="http://www.w3.org/1999/xlink">';
 
 		$all_files = array();
 		$index_terms = array();
